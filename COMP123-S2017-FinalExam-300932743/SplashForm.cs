3ce69143c71bb85c13c2d5b94e316899c07940d4
300932743 <300932743@PR-A315-35>
2017-08-17 12:54:02 -0400

    Added Splash Form

---
+++ COMP123-S2017-FinalExam-300932743/SplashForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Name: Saksham Tejpal
+ * Date: 8/17/17
+ * StudentID: 300932743
+ * Description: Splash Form to show prior to actual application
+ * Version: Added Splash Form
+ */
+
+namespace COMP123_S2017_FinalExam_300932743
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
