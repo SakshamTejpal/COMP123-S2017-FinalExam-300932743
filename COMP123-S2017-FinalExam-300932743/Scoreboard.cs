95964bc52262b3171d845af00e595b81cdef30fa
300932743 <300932743@PR-A315-35>
2017-08-17 13:13:16 -0400

    Created Scoreboard Class

--- COMP123-S2017-FinalExam-300932743/Scoreboard.cs
+++ COMP123-S2017-FinalExam-300932743/Scoreboard.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Name:Saksham Tejpal
+ * Date:8/17/17
+ * StudentID:300932743
+ * Description:Driver Class
+ * Version:0.1-Created Scoreboard Class
+ */
 namespace COMP123_S2017_FinalExam_300932743
 {
-    class Class1
+    class Scoreboard
     {
     }
 }
