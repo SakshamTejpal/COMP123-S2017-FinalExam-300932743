06dfbcdc53b56777268ec125d16b19c1d2703118
300932743 <300932743@PR-A315-35>
2017-08-17 13:31:44 -0400

    Added Feilds and Properties to Scoreboard Class

--- COMP123-S2017-FinalExam-300932743/Scoreboard.cs
+++ COMP123-S2017-FinalExam-300932743/Scoreboard.cs
@@ -3,16 +3,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 /*
  * Name:Saksham Tejpal
  * Date:8/17/17
  * StudentID:300932743
  * Description:Driver Class
- * Version:0.1-Created Scoreboard Class
+ * Version:0.2-Added Feilds and Properties to Scoreboard Class
  */
 namespace COMP123_S2017_FinalExam_300932743
-{
-    class Scoreboard
+{/// <summary>
+/// 
+/// </summary>
+    public class Scoreboard
     {
+        /// <summary>
+        /// Feilds for this class
+        /// </summary>
+        private TextBox _finalScoreTextBox;
+        private TextBox _scoreTextBox;
+        private TextBox _timeTextBox;
+        private int _score;
+        private int _time;
+        /// <summary>
+        /// Properties
+        /// </summary>
+        public TextBox FinalScoreTextBox
+        {
+            get
+            {
+                return this._finalScoreTextBox;
+            }
+            set
+            {
+                this._finalScoreTextBox = value;
+            }
+        }
+
+        public int Score
+        {
+            get
+            {
+                return this._score;
+            }
+            set
+            {
+                this._score = int.Parse(this.ScoreTextBox.Text);
+                this._score = int.Parse(this.FinalScoreTextBox.Text);
+            }
+        }
+        public TextBox ScoreTextBox
+        {
+            get
+            {
+                return this._scoreTextBox;
+            }
+            set
+            {
+                this._scoreTextBox = value;
+            }
+        }
+
+        public int Time
+        {
+            get
+            {
+                return this._time;
+            }
+            set
+            {
+                this._time = Convert.ToInt16(this.TimeTextBox);
+
+            }
+        }
+
+        public TextBox TimeTextBox
+        {
+            get
+            {
+                return this._timeTextBox;
+            }
+            set
+            {
+                this._timeTextBox = value;
+            }
+        }
+
     }
 }
